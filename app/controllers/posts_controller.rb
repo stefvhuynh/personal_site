6b8c992ba2affd5feff95e8b9d2bdc33e1832ce1
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-24 12:04:02 -0400

    Add header partial for all users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     
     if @post.update(post_params)
-      # redirect_to ...
+      redirect_to post_url(@post)
     else
       flash.now[:errors] = @post.errors.full_messages
       render :edit
